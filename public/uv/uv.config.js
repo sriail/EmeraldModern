749f8e5208cc0d265cd8df4e3de5844a52611401
sriail <mackinleymorrone@gmail.com>
2025-10-26 18:12:54 -0400

    Inject popup interception script into all pages
    Added popup interception script injection to config.

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,4 +1,3 @@
-/*global Ultraviolet*/
 self.__uv$config = {
   prefix: "/~/uv/",
   bare: "/w/",
@@ -9,4 +8,10 @@ self.__uv$config = {
   bundle: "/uv/uv.bundle.js",
   config: "/uv/uv.config.js",
   sw: "/uv/uv.sw.js",
+  
+  // Inject popup interception script into all pages
+  inject: [{
+    host: '*',
+    html: '<script src="/uv/uv-inject.js"></script>'
+  }]
 };
