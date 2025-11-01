43e8f35989bedbeceb6cbfbcf948abb54c7fb423
sriail <mackinleymorrone@gmail.com>
2025-11-01 14:45:48 -0400

    Remove duplicate Tab interface definition

--- src/components/homeComponents/tabbed.tsx
+++ src/components/homeComponents/tabbed.tsx
@@ -35,6 +35,25 @@ import {
 } from "../ui/select";
 import { Obfuscate } from "../obf";
 import { VERSION } from "@/constants";
+declare global {
+  interface Window {
+    sj?: {
+      init: (path: string) => Promise<ServiceWorkerRegistration>;
+      createFrame: (frame?: HTMLIFrameElement) => void;
+      encodeUrl: (url: string | URL) => string;
+      saveConfig: () => void;
+      modifyConfig: () => void;
+    };
+  }
+}
+
+interface Tab {
+  id: string;
+  title: string;
+  url: string;
+  favicon?: string;
+  isActive: boolean;
+}
 interface Tab {
   id: string;
   title: string;
